cd3efee0c85438f396356cc037ddeb573aca1e30
Luis Guillermo Cuevas García <lucuevas@deloittemx.com>
2017-06-21 16:24:12 -0500

    Initial Commit

---
+++ SellerAppNetworkingWL/SellerAppNetworkingWL.h
@@ -0,0 +1,19 @@
+//
+//  SellerAppNetworkingWL.h
+//  SellerAppNetworkingWL
+//
+//  Created by Luis Cuevas on 21/06/17.
+//  Copyright © 2017 Deloitte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SellerAppNetworkingWL.
+FOUNDATION_EXPORT double SellerAppNetworkingWLVersionNumber;
+
+//! Project version string for SellerAppNetworkingWL.
+FOUNDATION_EXPORT const unsigned char SellerAppNetworkingWLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SellerAppNetworkingWL/PublicHeader.h>
+
+
