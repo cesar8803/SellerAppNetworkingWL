034f580656ad5707efc9463855f4992b401c70df
Bernardino Guerrero Ibarra <bernardino.guerrero@gmail.com>
2017-10-09 18:54:09 -0500

    Last changes

---
+++ SellerAppNetworkingWL/WalletBalanceResponse.swift
@@ -0,0 +1,15 @@
+//
+//  WalletBalanceResponse.swift
+//  SellerAppNetworkingWL
+//
+//  Created by Bernardino Guerrero Ibarra on 10/9/17.
+//  Copyright © 2017 Deloitte. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+public class WalletBalanceResponseWL:Mappable{
+    public nc:String?
+    public sa:String
+}
